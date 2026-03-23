e54e6aa47761340a230c5d7aea89a2fd7450eebd
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-23 13:16:27 +0100

    create login and dashboard pages

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,5 +1,27 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div>
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full border p-2 mb-3 rounded"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full border p-2 mb-4 rounded"
+        />
+
+        <button className="w-full bg-indigo-500 text-white py-2 rounded">
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/features/dashboard/pages/Dashboard.tsx
+++ src/features/dashboard/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <div>Dashboard Page</div>;
 };
 
 export default Dashboard;
