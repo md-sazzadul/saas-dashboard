92d872688aa4c468cfdce41f8d736f78fcc7ea59
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-23 15:40:51 +0100

    include logics into login page

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -1,23 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { setToken } from "../../../utils/auth";
+import { loginApi } from "../api/authApi";
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      setError("");
+
+      const res = await loginApi({ email, password });
+
+      setToken(res.token);
+      navigate("/dashboard");
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <h2>Login</h2>
+    <div className="h-screen flex items-center justify-center bg-gray-50">
+      <div className="bg-white p-8 rounded-xl shadow w-96">
+        <h2 className="text-xl font-semibold mb-4">Login</h2>
+
+        {error && <p className="text-red-500 text-sm mb-3">{error}</p>}
 
         <input
           type="email"
-          placeholder="Email"
+          placeholder="admin@example.com"
           className="w-full border p-2 mb-3 rounded"
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
           type="password"
-          placeholder="Password"
+          placeholder="123456"
           className="w-full border p-2 mb-4 rounded"
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="w-full bg-indigo-500 text-white py-2 rounded">
-          Login
+        <button
+          onClick={handleLogin}
+          className="w-full bg-indigo-500 text-white py-2 rounded"
+        >
+          {loading ? "Logging in..." : "Login"}
         </button>
       </div>
     </div>
