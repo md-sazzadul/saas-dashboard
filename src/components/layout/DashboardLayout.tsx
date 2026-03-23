c2e8e66d5432a71920ad09fe117fe51bd8c452be
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-23 13:12:15 +0100

    setup dashoard layout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,5 +1,21 @@
+import { Outlet } from "react-router";
+
 const DashboardLayout = () => {
-  return <div>Dashboar Layout</div>;
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <aside className="w-64 bg-gray-900 text-white p-6">Sidebar</aside>
+
+      <div className="flex-1 flex flex-col">
+        <header className="h-16 bg-white border-b flex items-center px-6">
+          Navbar
+        </header>
+
+        <main className="p-6 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
