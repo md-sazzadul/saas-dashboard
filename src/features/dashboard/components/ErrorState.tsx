52f7f7023a7051327cac11bdb04673fb98d7f624
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-24 05:09:53 +0100

    create error state component

---
+++ src/features/dashboard/components/ErrorState.tsx
@@ -0,0 +1,9 @@
+const ErrorState = () => {
+  return (
+    <div className="text-center py-10">
+      <p className="text-red-500 font-semibold">Failed to load data</p>
+    </div>
+  );
+};
+
+export default ErrorState;
