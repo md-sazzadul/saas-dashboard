4ab9c4b81bc0403f20f47188f3bc36ce20546da5
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-24 05:08:20 +0100

    create skeleton loader component

---
+++ src/features/dashboard/components/Skeleton.tsx
@@ -0,0 +1,10 @@
+const Skeleton = () => {
+  return (
+    <div className="animate-pulse space-y-4">
+      <div className="h-6 bg-gray-200 rounded w-1/3"></div>
+      <div className="h-40 bg-gray-200 rounded"></div>
+    </div>
+  );
+};
+
+export default Skeleton;
